0c8a8993f87409fdf63bcadd1b2674347a2927ea
riya-patil <sd.riya.patil@gmail.com>
2022-11-27 20:54:02 -0800

    d

--- src/main/java/com/nighthawk/spring_portfolio/mvc/person/PersonApiController.java
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/person/PersonApiController.java
@@ -123,7 +123,7 @@ public class PersonApiController {
                     attributeMap.put(entry.getKey(), entry.getValue());
             }
             // Set Date and Attributes to SQL HashMap
-            Map<String, Map<String, Object>> date_map = new HashMap<>();
+            Map<String, Map<String, Object>> date_map = person.getStats();
             date_map.put( (String) stat_map.get("date"), attributeMap );
             person.setStats(date_map);  // BUG, needs to be customized to replace if existing or append if new
             repository.save(person);  // conclude by writing the stats updates
